095882d9f8f8b079d2ce2f519d9d6c9d97841688
KeenNjupt <45591391+KeenNjupt@users.noreply.github.com>
2021-11-25 21:16:20 +0800

    Create 数组.java

---
+++ 数组.java
@@ -0,0 +1,7 @@
+public static void main(String[] args) {
+		int[] a = new int[10]; // 变量a为数组变量，它只是放置数组元素的内存的管理者
+		int[] b = a; // 变量b也为数组变量，此赋值表示它会管理a管理的数组
+		b[0] = 5;   // 因为b和a管理同一个数组，b[0] = a[0]
+		System.out.println(a[0]); // 输出5
+		
+	}
