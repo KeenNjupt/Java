0f8c763860d314613301d05ac3ed99af175eff91
KeenNjupt <45591391+KeenNjupt@users.noreply.github.com>
2021-11-25 21:17:46 +0800

    Update 数组.java

--- 数组.java
+++ 数组.java
@@ -3,5 +3,5 @@ public static void main(String[] args) {
 		int[] b = a; // 变量b也为数组变量，此赋值表示它会管理a管理的数组
 		b[0] = 5;   // 因为b和a管理同一个数组，b[0] = a[0]
 		System.out.println(a[0]); // 输出5
-		
+		System.out.println(a.length); // 每个数组变量都有一个length变量表示数组的长度
 	}
